e0672084318fdca535b89815c21550624d8707ef
kimjunseoalta10 <111472691+kimjunseoalta10@users.noreply.github.com>
2023-03-29 11:26:59 +0900

    Add files via upload

---
+++ asd.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+import java.util.*;
+public class asd{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        //int a=10;
+        //if(a%2==0) 
+            //System.out.println(a+"은(는) 짝수입니다.");
+        //else
+           // System.out.println(a+"은(는) 홀수입니다.");
+        //int a=20,b=10;
+        //char op='*';
+        //if(op=='+')
+            //System.out.println(a+" + "+b+ " = "+(a+b));
+        //else if(op=='-')
+            //System.out.println(a+" - "+b+ " = "+(a-b));
+        //else if(op=='*')
+            //System.out.println(a+" * "+b+ " = "+(a*b));
+        //else if(op=='/')
+            //System.out.println(a+" / "+b+ " = "+(a/b));
+        //else
+            //System.out.println("사칙연산자가 아닙니다.");
+        //int a=20, b=10;
+        //char op='*';
+        //switch(op){
+            //case '+' : System.out.println(a+" + "+ b+ " = "+(a+b));
+            //case '-' : System.out.println(a+" - "+ b+ " = "+(a-b));
+            //case '*' : System.out.println(a+" * "+ b+ " = "+(a*b));
+            //case '/' : System.out.println(a+" / "+ b+ " = "+(a/b));
+            //default : System.out.println("사칙연산자가 아닙니다.");
+        //}
+        //int a=7;
+        //switch(a%3){
+            //case 0 : System.out.println(a+"은(는) 3의 배수입니다."); break;
+            //case 1 :
+            //case 2 : System.out.println(a+"은(는) 3의 배수가 아닙니다."); break; 
+        //}
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("달 입력 : ");
+            //int a=sc.nextInt();
+            //int day;
+            //switch(a){
+                //case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    //day=31; break;
+                //case 4: case 6: case 9: case 11:
+                    //day=30; break;
+                //default :
+                    //day=28;
+            //}
+            //System.out.println(a+"월은 "+day+"일입니다.");
+        //int even_sum =0, odd_sum=0;
+        //for(int i=1; i<=10; i++)
+            //if(i%2==0) even_sum+=i;
+            //else odd_sum+=i;
+        //System.out.println("1부터 10까지의 짝수 합은 " + even_sum +"이고, "+ "홀수 합은 " + odd_sum + "입니다.");
+        //int sum=0;
+        //System.out.print("1-2+3-4+5-6+7-8+9-10=");
+        //for(int i = 1; i<=10; i++)
+            //if(i%2==0) sum-=i;
+            //else sum+=i;
+        //System.out.println(sum);
+        //for(int i =1; i<=5; i++)
+        //{
+            //for(int j =1; j<=5; j++)
+            //{
+                //System.out.print(j + "\t");
+            //}
+            //System.out.print("\n");
+        //}
+        //for(int i=1; i<=5; i++)
+        //{
+            //for(int j=1; j<=i; j++)
+            //{
+                //System.out.print(j+"\t");
+            //}
+            //System.out.print("\n");
+        //}
+        //for(int i=1; i<=3; i++)
+        //{
+            //for(int j=1; j<=i; j++)
+            //{
+                //System.out.print("*"+"\t");
+            //}
+            //System.out.print("\n");
+        //}
+        for(int i =3; i>=1; i--)
+        {
+            for(int j=i; j>=1; j--)
+            {
+                System.out.print("*"+ "\t");
+            }
+            System.out.print("\n");
+        }
+
+
+    }
+}
