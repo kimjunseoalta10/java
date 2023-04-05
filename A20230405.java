f95ea38b1f34d6bbe299ba343a612f3ca2793285
kimjunseoalta10 <111472691+kimjunseoalta10@users.noreply.github.com>
2023-04-05 11:06:43 +0900

    Add files via upload

---
+++ A20230405.java
@@ -0,0 +1,53 @@
+class Student{
+//	public String name;
+//	int grade ;
+//	int ban;
+//	int number;
+//	public String telephone;
+	
+	static String school="hangang";
+	String name;
+	int grade;
+	
+}
+
+
+public class A20230405 {
+	public static void main(String[] args) {
+//		int i=1;
+//		while(i<=5)
+//		{
+//			System.out.print(i+"\t");
+//			i++;
+//		}
+		
+//		int i=1, sum=0;
+//		System.out.print("1-2+3-4+5-6+7-8+9-10=");
+//		while(i<=10) {
+//			if(i%2==0) {
+//				sum-=i;
+//			}
+//			else {
+//				sum+=i;
+//			}
+//			i++;
+//		}
+//		System.out.print(sum);
+		
+//		int i=5;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		}while(i>10);
+		
+//		for(int i =1; i<=10; i++) {
+//			System.out.print(i+"\t");
+//			if(i==5) break;
+//		}
+		
+//		for(int i =1; i<=10; i++) {
+//		if(i==5) continue;
+//		System.out.print(i+"\t");
+//		}
+	}
+}
---
+++ A20230405Exam.java
@@ -0,0 +1,46 @@
+//class Student1{
+//	 String name;
+//	 int grade;
+//	 int ban;
+//	 int nuber;
+//	 String telephone;
+//	 
+//	 public void Student1() {
+//		 
+//		 grade = 2;
+//		 name = "김현우";
+//	 }
+//	 
+//	 public void disp() {
+//		 
+//		 System.out.print("학생의 이름은 "+name+ "이고"+grade+"학년입니다");
+//			
+//		 
+//	 }
+//	
+//	
+//}
+//
+//public class A20230405Exam {
+//	public static void main(String[] args) {
+//		
+//		Student1 st = new Student1();
+//		
+//		st.name = "김현우";
+//		st.grade = 2;
+//		
+//		st.disp();
+//		
+//	}
+//}
+public class A20230405Exam{
+	public static void main(String[] args) {
+		Student kim = new Student();
+		Student jang = new Student();
+		Student bak = new Student();
+//		kim.name="김현우";
+//		kim.grade =2;
+//		kim.telephone = "111-1111";
+//		System.out.println("학생의 이름은"+kim.name+"이고,"+kim.grade+"학년입니다.");
+	}
+}
