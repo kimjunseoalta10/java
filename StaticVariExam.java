fd0f27bfaa7f63d290a54a8b9b0d2c0b112463a1
kimjunseoalta10 <111472691+kimjunseoalta10@users.noreply.github.com>
2023-05-03 10:34:14 +0900

    Add files via upload

---
+++ StaticVariExam.java
@@ -0,0 +1,10 @@
+
+public class StaticVariExam {
+//	public static void main(String[] args) {
+//		Student kim = new Student();
+//		Student jang = new Student();
+//		Student bak = new Student();
+//		System.out.println("생성되 학생 객체 수는 "+Student.count+"입니다.");
+//	}
+
+}
---
+++ Student.java
@@ -0,0 +1,40 @@
+
+public class Student {
+//	String name;
+//	int grade;
+//	static int count = 0;
+//	public Student() {
+//		count++;
+//	}
+	
+//	String name ;
+//	int grade ;
+//	static int count = 0;
+//	public Student() {
+//		count++;
+//	}
+//	public static int getCount() {
+//		return count;
+//	}
+	
+	private String name ;
+	private int grade ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String n) {
+		name = n;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int g) {
+		grade = g;
+	}
+	
+	public void print() {
+		System.out.println("학생의 이름은 "+name+"이고"+grade+"학년입니다.");
+	}
+	
+
+}
